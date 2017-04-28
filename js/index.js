7cd19177b6c48af8e4465084568f2950a8b60d82
Zhenle Zhu <FivePercent@ZhuZhenles-Mac-mini.local>
2017-04-28 00:32:17 -0700

    hover

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ $("#hamburger-menu").on("click", function(){
 	//document.getElementById("hamburger-menu-handle").classList.toggle("active");
 	$("#hamburger-menu-handle").toggleClass("active");
 	$("#pull-down-menu").toggleClass("is-open");
+	$("#overlay").toggleClass("is-open");
 });
 
 $(function() {
