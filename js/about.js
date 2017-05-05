62d36026b75d9ca7e40aa4300b8a50866f123401
Zhenle Zhu <FivePercent@ZhuZhenles-Mac-mini.local>
2017-05-04 23:44:05 -0700

    added project_page

--- js/about.js
+++ js/about.js
@@ -1,37 +1,18 @@
+$("#hamburger-menu").on("click", function(){
+	//document.getElementById("hamburger-menu-handle").classList.toggle("active");
+	$("#hamburger-menu-handle").toggleClass("active");
+	$("#pull-down-menu").toggleClass("is-open");
+	$("#overlay").toggleClass("is-open");
+});
 
-var xmlhttp = new XMLHttpRequest();
-var about;
-xmlhttp.onreadystatechange = function(){
- 	if(this.readyState == 4 && this.status == 200){
- 		about = JSON.parse(this.responseText);
- 		display();
-	}
-};
-xmlhttp.open("GET", "data/about.json", true);
-xmlhttp.send();
+$(function() {
+	$(window).scroll(function() {
+		var move = $(window).scrollTop();
+		if(move>100){
+			$(".nav-header").addClass("nav-header-shrink").animate();
 
-function display(){
-	var formattedaboutpic = HTMLaboutpic.replace("%data%", "images/aboutpic.jpg");
-	var formattedaboutwelcome = HTMLaboutwelcome.replace("%data%", about.welcomemessage);
-	var formattedintroduction = HTMLaboutintroduction.replace("%data%", about.introduction);
-
-	$("#container").append(HTMLabouttitle);
-	$("#container").append(HTMLaboutmainbar);
-	$("#container").append(HTMLaboutsidebar);
-	$("#container").append(HTMLboundry);
-	$("#container").append(formattedaboutwelcome);
-	$("#container").append(formattedintroduction);
-
-
-	$("#mainbar").append(formattedaboutpic);
-	$("#sidebar").append(HTMLabouttags);
-	$("#tags").append(HTMLabouttagtitle);
-
-	if(about.featuredtags.length>0){
-		for(var i=0; i<about.featuredtags.length; i++){
-			var formattedabouttag = HTMLabouttag.replace("%data%", about.featuredtags[i]);
-			$("#tags").append(formattedabouttag);
+		}else{
+			$(".nav-header").removeClass("nav-header-shrink");
 		}
-	}
-
-}
+	 });
+});
--- js/index.js
+++ js/index.js
@@ -9,37 +9,28 @@ $(function() {
 	$(window).scroll(function() {
 		var move = $(window).scrollTop();
 		if(move>100){
-			$(".title").css({bottom: '0%'});
+			$(".title").css({bottom: '25%'});
 			$(".item-0").css({top: '5%'});
 			$(".item-1").css({bottom: '65%'});
 			$(".item-2").css({bottom: '50%'});
 			$(".item-3").css({bottom: '40%'});
-			$(".item-4").css({bottom: '20%'});
+			$(".item-4").css({bottom: '25%'});
 			$(".item-5").css({bottom: '65%'});
 			$(".item-6").css({bottom: '42%'});
-			$(".item-7").css({bottom: '20%'});
+			$(".item-7").css({bottom: '25%'});
 			$(".nav-header").addClass("nav-header-shrink").animate();
 
 		}else{
-			$(".title").css({bottom: '40%'});
+			$(".title").css({bottom: '50%'});
 			$(".item-0").css({top: '12%'});
 			$(".item-1").css({bottom: '75%'});
 			$(".item-2").css({bottom: '60%'});
 			$(".item-3").css({bottom: '50%'});
-			$(".item-4").css({bottom: '30%'});
+			$(".item-4").css({bottom: '35%'});
 			$(".item-5").css({bottom: '75%'});
 			$(".item-6").css({bottom: '52%'});
-			$(".item-7").css({bottom: '30%'});
+			$(".item-7").css({bottom: '35%'});
 			$(".nav-header").removeClass("nav-header-shrink");
 		}
-		console.log(move);
-		// var percentage = 30-(move/600)*20;
-		// $(".item-1").animate({bottom: percentage+"%"});
-		// $(".item-2").css({"transform": "translate(0px,"+move+"px)"});
-		// $(".item-3").css({"transform": "translate(0px,"+move+"px)"});
-		// $(".item-4").css({"transform": "translate(0px,"+move+"px)"});
-		// $(".item-5").css({"transform": "translate(0px,"+move+"px)"});
-		// $(".item-6").css({"transform": "translate(0px,"+move+"px)"});
-		// $(".item-7").css({"transform": "translate(0px,"+move+"px)"});
 	 });
 });
